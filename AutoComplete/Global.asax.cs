3d3fd1312894428c892d63585e2bb56737ad2b7c
Vijay Paliwal <Vijay.Paliwal@hotmail.com>
2013-03-06 14:26:21 +0530

    global.asax

--- AutoComplete/Global.asax.cs
+++ AutoComplete/Global.asax.cs
@@ -9,9 +9,11 @@ namespace AutoComplete
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
-
+    
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static bool showMessage = false;
+        public static string messageTxt = string.Empty;
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
